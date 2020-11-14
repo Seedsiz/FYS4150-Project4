c4a8fc2185da6a264d2d74c4924333a41aefb5d8
Sidra Rashid <sidrarashid@eduroam-193-157-236-144.wlan.uio.no>
2020-11-14 17:05:02 +0100

    Trying to check threads.

--- code-and-results/main.cpp
+++ code-and-results/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <armadillo>
 #include <omp.h>
+#include <stdio.h>
 
 using namespace std;
 using namespace arma;
@@ -20,7 +21,7 @@ void menu(){
   int L; int MC;
   double T_start, T_end;
   int n_T;
-  int num_threads;
+  int numthreads;
 
   cout << "Enter integer number of spin particles for each axis:" << " ";
   cin >> L;
@@ -33,7 +34,7 @@ void menu(){
   cout << "Enter integer number of MC cycles:"  << " ";
   cin >> MC;
   cout << "Enter integer number of threads:"  << " ";
-  cin >> num_threads;
+  cin >> numthreads;
 
   //Tryout random generator
   //MonteCarlo mysolver;
@@ -48,21 +49,21 @@ void menu(){
 
   // set up T_vec for start and end for each node
   // temperature vector with T_start, T_end for nodes
-  T_vec = linspace<vec>(T_start, T_end, num_threads*2);
+  T_vec = linspace<vec>(T_start, T_end, numthreads*2);
   IsingModel2D model; // initate class object;
 
   double start;
   double end;
   start = omp_get_wtime();
-  omp_set_num_threads(num_threads);
-  # pragma omp parallel for default(shared) private (temps_i);
-
-  for (temps_i = 0; temps_i < num_threads;temps_i++){
+  omp_set_num_threads(numthreads);
+  #pragma omp parallel for default(none) num_threads(numthreads) private(temps_i)
+  for (temps_i = 0; temps_i < numthreads; temps_i++){
     T_start = T_vec(2*temps_i);
     T_end = T_vec(2*temps_i+1);
     model.init(L, T_start,T_end, n_T, MC);
     sol = model.solve();
-    cout << sol;
+    printf("Thread rank: %d\n", omp_get_thread_num());
+
   }
 
   end = omp_get_wtime();
